bbe929f3d8eb7bfcd5e7cd17b23fbf8d584b41d5
jaimish <jaimish@hopeleaftechnologies.com>
2025-07-29 13:59:20 +0530

    add condition for pdf-client-logo

--- resources/views/components/pdf-client-logo.blade.php
+++ resources/views/components/pdf-client-logo.blade.php
@@ -2,7 +2,8 @@
     <img class="float-left" style="width:200px; height: 100px"
          src='data:image/png;base64,
             {{
-                \Illuminate\Support\Facades\Storage::disk('public')->exists(auth()->user()->logo_path)
+                 (!empty(auth()->user()->logo_path)
+                 && \Illuminate\Support\Facades\Storage::disk('public')->exists(auth()->user()->logo_path))
                 ? base64_encode(\Illuminate\Support\Facades\Storage::disk('public')->get(auth()->user()->logo_path))
                 : base64_encode(file_get_contents(public_path('images/fit_test_logo_placeholder.jpeg')))
             }}'
